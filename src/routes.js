d59bb845154ca3d114344db5d3e7c61c08461557
mizuti <kyle.rentschler1@gmail.com>
2017-01-29 01:02:01 -0500

    added routes and styles

---
+++ src/routes.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import{ Route, IndexRoute } from 'react-router';
+import App from './components/app';
+import HomePage from './components/home/HomePage';
+import AboutPage from './components/about/AboutPage';
+import Dashboard from './components/dashboard/DashboardPage';
+
+export default (
+  <Route path="/" component={App}>
+    <IndexRoute component={HomePage}/>
+    <Route path="dashboard" component={Dashboard}/>
+    <Route path="about" component={AboutPage} />
+  </Route>
+);
+
